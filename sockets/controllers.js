7af5f7e7076bbde0f75caad4dd898bac67cce0aa
Antonio-Josen <19170628@itculiacan.edu.mx>
2024-07-11 12:13:52 -0700

    First Commit

---
+++ sockets/controllers.js
@@ -0,0 +1,22 @@
+
+
+const socketsController = (socket) => {
+
+    console.log('Cliente conectado',socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado', socket.id);
+    })
+
+    socket.on('enviar-mensaje', (payload,callback) => {
+
+        const id =123456;
+        callback({ id,fecha: new Date().getTime() });
+        socket.broadcast.emit('enviar-mensaje', payload);
+    })
+}
+
+module.exports = {
+
+    socketsController
+}
